9dd34536c21d3e96e5707f40e5894a3575cf97a8
sarah <sarahizabelp@gmail.com>
2018-08-05 21:32:20 -0300

    primeiro commit

---
+++ users/users.router.ts
@@ -0,0 +1,71 @@
+import * as restify from 'restify';
+import { Router } from '../common/router';
+import { NotFoundError } from 'restify-errors';
+import { User } from './users.model';
+
+class UsersRouter extends Router {
+
+  constructor() {
+    super()
+    this.on('beforeRender', document => {
+      document.password = undefined;
+    });
+  }
+
+  applyRoutes(application: restify.Server) {
+
+    application.get('/users', (req, res, next) => {
+      User.find()
+        .then(this.render(res, next))
+        .catch(next);
+    });
+
+    application.get('/users/:id', (req, res, next) => {
+      User.findById(req.params.id)
+        .then(this.render(res, next))
+        .catch(next);
+    });
+
+    application.post('/users', (req, res, next) => {
+      let user = new User(req.body);
+      user.save()
+        .then(this.render(res, next))
+        .catch(next);
+    });
+
+    application.put('/users/:id', (req, res, next) => {
+      const options = {runValidators: true, overwrite: true};
+      User.update({_id: req.params.id}, req.body, options)
+      .exec().then(result => {
+        if(result.n) {
+          return User.findById(req.params.id);
+        } else {
+          throw new NotFoundError('Documento não encontrado');
+        }
+      }).then(this.render(res, next))
+        .catch(next);
+    });
+
+    application.patch('/users/:id', (req, res, next) => {
+      const options = {runValidators: true, new: true};
+      User.findByIdAndUpdate(req.params.id, req.body, options)
+        .then(this.render(res, next))
+        .catch(next);
+    });
+
+    application.del('/users/:id', (req, res, next) => {
+      User.remove({_id: req.params.id})
+      .exec().then((result : any) => {
+        if(result.result.n) {
+          res.send(204);
+        } else {
+          throw new NotFoundError('Documento não encontrado');
+        }
+        return next();
+      }).catch(next);
+    });
+  }
+
+}
+
+export const usersRouter = new UsersRouter();
